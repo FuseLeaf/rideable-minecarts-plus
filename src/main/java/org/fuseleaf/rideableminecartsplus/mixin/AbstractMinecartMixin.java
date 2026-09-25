4f1779943c129ddd194e49c6739163dda158e965
快乐凤爪王 <165921128+KSSJW@users.noreply.github.com>
2026-09-25 20:14:01 +0800

    Update version to 1.0.1+26.3 (#4)
    * fix: prevent mounting occupied minecarts
    
    * refactor: remove redundant mixin methods
    
    * chore: bump version to 1.0.1+26.3

--- src/main/java/org/fuseleaf/rideableminecartsplus/mixin/AbstractMinecartMixin.java
+++ src/main/java/org/fuseleaf/rideableminecartsplus/mixin/AbstractMinecartMixin.java
@@ -14,19 +14,12 @@ import net.minecraft.world.phys.Vec3;
 @Mixin(AbstractMinecart.class)
 public class AbstractMinecartMixin {
 
-    @Inject(method = "isRideable", at = @At("TAIL"), cancellable = true)
-    public void injectIsRideable(CallbackInfoReturnable<Boolean> cir) {
-        if (!((AbstractMinecart)(Object)this instanceof Minecart)) {
-            cir.setReturnValue(true);
-        }
-    }
-
     @Inject(method = "getPassengerAttachmentPoint", at = @At("RETURN"), cancellable = true)
     private void injectGetPassengerAttachmentPoint(CallbackInfoReturnable<Vec3> cir) {
         AbstractMinecart cart = (AbstractMinecart)(Object)this;
         Vec3 vec3 = cir.getReturnValue();
 
-        if (!((AbstractMinecart)(Object)this instanceof Minecart)) {
+        if (!(cart instanceof Minecart)) {
             cir.setReturnValue(Ride.adjustPassengerPosition(cart, vec3));
         }
     }

fdd676be9a5989c4c730bd5ef717f77e5686482b
快乐凤爪王 <165921128+KSSJW@users.noreply.github.com>
2026-09-25 11:42:35 +0800

    1.0.0+1.21.forge (#5)
    * refactor: port to Forge
    
    * chore: update .gitignore

--- src/main/java/org/fuseleaf/rideableminecartsplus/mixin/AbstractMinecartMixin.java
+++ src/main/java/org/fuseleaf/rideableminecartsplus/mixin/AbstractMinecartMixin.java
@@ -7,19 +7,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.minecraft.world.entity.vehicle.minecart.AbstractMinecart;
-import net.minecraft.world.entity.vehicle.minecart.Minecart;
+import net.minecraft.world.entity.vehicle.AbstractMinecart;
+import net.minecraft.world.entity.vehicle.Minecart;
 import net.minecraft.world.phys.Vec3;
 
 @Mixin(AbstractMinecart.class)
 public class AbstractMinecartMixin {
-
-    @Inject(method = "isRideable", at = @At("TAIL"), cancellable = true)
-    public void injectIsRideable(CallbackInfoReturnable<Boolean> cir) {
-        if (!((AbstractMinecart)(Object)this instanceof Minecart)) {
-            cir.setReturnValue(true);
-        }
-    }
 
     @Inject(method = "getPassengerAttachmentPoint", at = @At("RETURN"), cancellable = true)
     private void injectGetPassengerAttachmentPoint(CallbackInfoReturnable<Vec3> cir) {

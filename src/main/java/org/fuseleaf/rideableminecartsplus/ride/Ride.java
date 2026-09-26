bd47c65ee104142fc2b99eb90df70c8cad754fb8
快乐凤爪王 <165921128+KSSJW@users.noreply.github.com>
2026-09-26 15:42:58 +0800

    1.0.2+26.3 (#11)

--- src/main/java/org/fuseleaf/rideableminecartsplus/ride/Ride.java
+++ src/main/java/org/fuseleaf/rideableminecartsplus/ride/Ride.java
@@ -20,7 +20,11 @@ public class Ride {
             double rideY = aabb.minY + (aabb.maxY - aabb.minY) * 0.9;
             double hitY = hitResult.getLocation().y;
 
-            if (hitY < rideY && !entity.isVehicle()) {
+            if (
+                hitY < rideY
+                && !player.isCrouching()
+                && !entity.isVehicle()
+            ) {
                 player.startRiding(entity, true, true);
 
                 return InteractionResult.SUCCESS;
